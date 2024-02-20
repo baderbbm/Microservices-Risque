60dac9e0a306cc53ff4123395c356528b4950f4e
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-20 13:06:02 +0100

    Update DiabetesRiskController.java

--- src/main/java/com/microservices/controller/DiabetesRiskController.java
+++ src/main/java/com/microservices/controller/DiabetesRiskController.java
@@ -14,23 +14,25 @@ import org.springframework.web.client.RestTemplate;
 public class DiabetesRiskController {
 	private final String urlMicroserviceGateway = "http://localhost:8081";
 
+	@Autowired
+	private DiabetesRiskCalculationService diabetesRiskCalculationService;
 
-    @Autowired
-    private DiabetesRiskCalculationService diabetesRiskCalculationService;
+	@GetMapping("/diabetes-risk/patients/{patientId}")
+	public ResponseEntity<String> getDiabetesRisk(@PathVariable Long patientId) {
+		RestTemplate restTemplate = new RestTemplate();
+		// Récupérer les détails du patient
+		PatientDTO patient = restTemplate.getForObject(urlMicroserviceGateway + "/patients/" + patientId,
+				PatientDTO.class);
 
-    @GetMapping("/diabetes-risk/patients/{patientId}")
-    public ResponseEntity<String> getDiabetesRisk(@PathVariable Long patientId) {
-    	RestTemplate restTemplate = new RestTemplate();
-    	// Récupérer les détails du patient
-        PatientDTO patient = restTemplate.getForObject(urlMicroserviceGateway + "/patients/" + patientId, PatientDTO.class);
-        
-        // Récupérer les notes du médecin associées au patient
-        ResponseEntity<MedecinNoteDTO[]> responseEntity = restTemplate.getForEntity(urlMicroserviceGateway + "/medecin/notes/" + patientId, MedecinNoteDTO[].class);
-        MedecinNoteDTO[] medecinNotes = responseEntity.getBody();
-    	
-        // Calculer le risque de diabète en utilisant le service de calcul du risque de diabète
-        String riskLevel = diabetesRiskCalculationService.calculateDiabetesRisk(patient, medecinNotes);
+		// Récupérer les notes du médecin associées au patient
+		ResponseEntity<MedecinNoteDTO[]> responseEntity = restTemplate
+				.getForEntity(urlMicroserviceGateway + "/medecin/notes/" + patientId, MedecinNoteDTO[].class);
+		MedecinNoteDTO[] medecinNotes = responseEntity.getBody();
 
-        return ResponseEntity.ok(riskLevel);
-    }
+		// Calculer le risque de diabète en utilisant le service de calcul du risque de
+		// diabète
+		String riskLevel = diabetesRiskCalculationService.calculateDiabetesRisk(patient, medecinNotes);
+
+		return ResponseEntity.ok(riskLevel);
+	}
 }
