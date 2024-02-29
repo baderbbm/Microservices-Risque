da7158fb6e5be6366087bc112f6a13451fb2c0f5
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-29 23:45:41 +0100

    Update DiabetesRiskController.java

--- src/main/java/com/microservices/controller/DiabetesRiskController.java
+++ src/main/java/com/microservices/controller/DiabetesRiskController.java
@@ -4,6 +4,9 @@ import com.microservices.dto.MedecinNoteDTO;
 import com.microservices.dto.PatientDTO;
 import com.microservices.services.DiabetesRiskCalculationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,19 +22,28 @@ public class DiabetesRiskController {
 
 	@GetMapping("/diabetes-risk/patients/{patientId}")
 	public ResponseEntity<String> getDiabetesRisk(@PathVariable Long patientId) {
-		RestTemplate restTemplate = new RestTemplate();
+
 		// Récupérer les détails du patient
-		PatientDTO patient = restTemplate.getForObject(urlMicroserviceGateway + "/patients/" + patientId,
-				PatientDTO.class);
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBasicAuth("pra", "pra");
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+
+		// Récupérer les détails du patient depuis le microservice via la gateway
+		ResponseEntity<PatientDTO> patientResponse = restTemplate
+				.exchange(urlMicroserviceGateway + "/patients/" + patientId, HttpMethod.GET, entity, PatientDTO.class);
 
 		// Récupérer les notes du médecin associées au patient
-		ResponseEntity<MedecinNoteDTO[]> responseEntity = restTemplate
-				.getForEntity(urlMicroserviceGateway + "/medecin/notes/" + patientId, MedecinNoteDTO[].class);
-		MedecinNoteDTO[] medecinNotes = responseEntity.getBody();
+
+		ResponseEntity<MedecinNoteDTO[]> notesResponse = restTemplate.exchange(
+				urlMicroserviceGateway + "/medecin/notes/" + patientId, HttpMethod.GET, entity, MedecinNoteDTO[].class);
+
+		MedecinNoteDTO[] medecinNotes = notesResponse.getBody();
 
 		// Calculer le risque de diabète en utilisant le service de calcul du risque de
 		// diabète
-		String riskLevel = diabetesRiskCalculationService.calculateDiabetesRisk(patient, medecinNotes);
+		String riskLevel = diabetesRiskCalculationService.calculateDiabetesRisk(patientResponse.getBody(),
+				medecinNotes);
 
 		return ResponseEntity.ok(riskLevel);
 	}
